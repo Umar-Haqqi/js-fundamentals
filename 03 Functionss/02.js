5eed609a7c80e22a6276f5dd93070855e9569dca
Umar-Haqqi <umer1562001@gmail.com>
2023-12-13 15:51:34 +0500

    functions with object and array

---
+++ 03 Functionss/02.js
@@ -0,0 +1,27 @@
+// function func(...val) {
+//     return val;
+// }
+// const result = func(100, 200, 300);
+// console.log(result);
+
+
+
+// --- function with objects
+// const userInfo = {
+//     username: "Ali",
+//     passwords: "123"
+// }
+// function handleObject(anyObj) {
+//     console.log(`Username: ${anyObj.username} and password: ${anyObj.password}`);
+// }
+// handleObject(userInfo);
+
+
+
+// --- function with array
+// const myArr = [199, 299, 399, 499];
+// function returnSecondValue(getArray) {
+//     return getArray[1];
+// }
+// const output = returnSecondValue(myArr);
+// console.log(output);
