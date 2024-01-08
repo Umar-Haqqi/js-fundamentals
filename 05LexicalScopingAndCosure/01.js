f77de0ba30b1939916f1e9c0932f3d2e9f31455f
Umar-Haqqi <umer1562001@gmail.com>
2024-01-08 13:17:02 +0500

    lexical scoping in javascript

---
+++ 05LexicalScopingAndCosure/01.js
@@ -0,0 +1,21 @@
+// Lexical scoping
+
+function outer() {
+    const outerVar = 10;
+
+    function innerOne() {
+        console.log("outerVar in innerOne: ", outerVar);
+    }
+
+    function innerTwo() {
+        console.log("innerOneVar in innerTwo: ", innerOneVar);
+    }
+
+    innerOne();
+    innerTwo();
+}
+
+outer();
+
+
+// in lexical scoping, child can access parent variables,but parent cannot access child variables, and also sibling cannot access each other variables
